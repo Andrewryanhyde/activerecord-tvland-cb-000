3b11e7f094e088689e854d4b1d1063bf3672424e
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-13 15:20:55 +0000

    automatically backed up by learn

--- db/migrate/008_add_day_and_season_and_genre_to_show.rb
+++
@@ -1,7 +0,0 @@
-class AddDayAndSeasonAndGenreToShow < ActiveRecord::Migration[4.2]
-  def change
-    add_column(:shows,:day,:text)
-    add_column(:shows,:season,:text)
-    add_column(:shows,:genre,:text)
-  end
-end
